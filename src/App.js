4502e3ab408405c2f8a8988e4a6d2dc61489527e
wongyunlam <https://github.com/wongyunlam/>
2022-03-12 01:33:00 +0800

    ecommerce setup-3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <>
-                    <h1>kick your breats</h1>
+                    <h1>Ecommerce</h1>
                 </>
             </header>
         </div>
